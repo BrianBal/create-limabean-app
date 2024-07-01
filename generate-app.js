18ea4d87a1d28c2d3fcf02a7aeb787a434abb5ce
Brian Bal <briantbal@gmail.com>
2024-06-30 21:34:35 -0400

    updated generate script

--- generate-app.js
+++ generate-app.js
@@ -1,6 +1,6 @@
-import { execSync } from "child_process"
-import path from "path"
-import fs from "fs"
+const { execSync } = require("child_process")
+const path = require("path")
+const fs = require("fs")
 
 if (process.argv.length < 3) {
     console.log("You have to provide a name to your app.")
